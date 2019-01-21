99a6996ed78dea8cdc2608a9c233d4b257c47bf5
anthager <anthage@student.chalmers.se>
2019-01-21 23:16:29 +0100

    added a algorithm for determine the ultimate backgroud color with a neural network

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const calcColors = () => {
+  const colors = ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
+  let color = ""
+  for (let i = 0; i < 6; i++) {
+    color += colors[Math.floor(Math.random() * colors.length)]
+  }
+  return color
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src="anton.png" className="anton"></img>
-        <span>.</span>
-        <img src="pizza.png" className="pizza"></img>
+      <div className="App" style={{"background-color": `#${calcColors()}`}}>
+        <img src="anton.png" className="anton" alt="Anton"></img>
+        <span style={{"color": `#${calcColors()}`}}>.</span>
+        <img src="pizza.png" className="pizza" alt="Pizza"></img>
       </div>
     );
   }
